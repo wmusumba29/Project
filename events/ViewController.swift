8b8538599acb94102f12b02465e92e405ed5f81b
William Musumba <william@Williams-MacBook-Pro.local>
2017-12-11 13:13:05 +0000

    Initial Commit

---
+++ events/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  events
+//
+//  Created by William Musumba on 11/12/2017.
+//  Copyright © 2017 William Musumba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
