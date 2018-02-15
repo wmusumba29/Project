ec98ddc22aa2f5b0f98a337b79bb184198d1b72d
William <william@Williams-MacBook-Pro.local>
2018-02-15 16:33:26 +0000

    Completed favourite list and cooment section for venues

---
+++ Extensions/UIViewExt.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  events
+//
+//  Created by William Musumba on 15/02/2018.
+//  Copyright © 2018 William Musumba. All rights reserved.
+//
+
+import Foundation
---
+++ Model/Review.swift
@@ -0,0 +1,9 @@
+//
+//  Review.swift
+//  events
+//
+//  Created by William Musumba on 14/02/2018.
+//  Copyright © 2018 William Musumba. All rights reserved.
+//
+
+import Foundation
