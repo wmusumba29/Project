feadaf2732e85b9853d025b4b20114ba62a458d3
William <william@Williams-MacBook-Pro.local>
2018-02-26 16:55:13 +0000

    Location of venues interface completed now just add in search for it and possible view for venues

---
+++ Controller/LocationVC.swift
@@ -0,0 +1,35 @@
+//
+//  LocationVC.swift
+//  events
+//
+//  Created by William Musumba on 25/02/2018.
+//  Copyright © 2018 William Musumba. All rights reserved.
+//
+
+import UIKit
+
+class LocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ View/VenuePin.swift
@@ -0,0 +1,9 @@
+//
+//  VenuePin.swift
+//  events
+//
+//  Created by William Musumba on 26/02/2018.
+//  Copyright © 2018 William Musumba. All rights reserved.
+//
+
+import Foundation
